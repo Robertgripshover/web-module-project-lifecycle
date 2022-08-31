bf45bfcb4cf5f7c7a85220ad05b419dd16cc3857
Robert Gripshover <robertgrips@gmail.com>
2022-08-31 10:50:39 -0400

    wip

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -2,6 +2,26 @@ import React from 'react'
 
 export default class Form extends React.Component {
   render() {
-    return null
+
+    const {} =  this.props
+
+    return (
+      <>
+        <form id='todoForm' onSubmit={this.props.onTodoFormSubmit}>
+        <input
+           value={this.props.state.todoNameInput}
+           onChange={this.props.onTodoNameInputChange}
+           type='text'
+           placeholder='Type todo'>
+       </input>
+        <input type='submit'></input>
+         </form>
+        <button
+          onClick={this.props.toggleDisplayCompleteds}
+        >
+            {this.props.state.displayCompleteds ? "Hide" : "Show"} Completed
+        </button>
+      </>
+    )
   }
 }
